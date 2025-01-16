0f8c55f03030a5a7c0a8052131e28323f1cf77c2
dragent <alexiscau@gmail.com>
2025-01-16 10:45:49 +0100

    Update ( Analyze feels ) : Push for more testing

--- README.md
+++ README.md
@@ -5,34 +5,41 @@
 ### Énoncé :
 
 Créez une application conversationnelle qui permet à l'utilisateur de dialoguer avec un chatbot. Pour chaque message de l'utilisateur et chaque réponse du chatbot, l'application doit :
+
 - Afficher le message original.
 - Afficher la traduction anglaise du message.
 - Afficher le sentiment exprimé dans le message (positif, négatif, neutre).
 
 Détails :
+
 - Utilisez un modèle de chatbot pré-entraîné de Hugging Face (ex: microsoft/DialoGPT-medium).
 - Utilisez un modèle de traduction pré-entraîné de Hugging Face (ex: Helsinki-NLP/opus-mt-fr-en).
 - Utilisez un modèle d'analyse de sentiment pré-entraîné de Hugging Face (ex: nlptown/bert-base-multilingual-uncased-sentiment).
 
 Description détaillée :
+
 - L'utilisateur pourra dialoguer avec un chatbot via une interface Gradio. Pour chaque message de l'utilisateur et chaque réponse du chatbot, l'application affichera le message original, sa traduction anglaise et le sentiment exprimé (positif, négatif, neutre). L'analyse de sentiment et la traduction seront effectuées en temps réel grâce aux modèles Hugging Face.
 
 Bibliothèques :
+
 - transformers
 - gradio
 
 ### Calendrier :
 
 Jour 1 :
+
 - Configuration de l'environnement et installation des bibliothèques.
 - Chargement et test du modèle de chatbot.
 - Implémentation de la conversation de base avec le chatbot.
 
 Jour 2 :
+
 - Chargement et test des modèles de traduction et d'analyse de sentiment.
 - Intégration de la traduction et de l'analyse de sentiment pour chaque message.
 
 Jour 3 :
+
 - Création de l'interface utilisateur avec Gradio.
 - Affichage des messages, traductions et sentiments dans l'interface.
 - Écriture de tests unitaires et du fichier README.md.
@@ -50,7 +57,7 @@ Jour 3 :
 - windows
   `.venv/Scripts/activate` or `.venv/Scripts/activate.ps1`
 - bash(windows)
-`source .venv/Scripts/activate`
+  `source .venv/Scripts/activate`
 
 **Install the librairies**
 
@@ -61,3 +68,10 @@ Jour 3 :
 ## Exécution
 
 SOON
+
+
+## Annexes
+
+### Database
+
+- Database of emotions from text :  [https://huggingface.co/datasets/google-research-datasets/go_emotions](https://huggingface.co/datasets/google-research-datasets/go_emotions "https://huggingface.co/datasets/google-research-datasets/go_emotions")
---
+++ components/models/sentiment_analysis/Dlsta.py
@@ -0,0 +1,78 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+import tensorflow as tf
+
+
+class Dlsta:
+    def __init__(self):
+        self.model = None
+
+    ############################################################################
+    ############################# Train the model ##############################
+    ############################################################################
+    def train_model(self, sequences, y):
+
+        x = pad_sequences(sequences, padding="post")
+        x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.9, random_state=42)
+        print(len(x_train))
+        
+        x_train = np.array(x_train)
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(input_dim=30000, output_dim=256),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+            tf.keras.layers.GlobalMaxPooling1D(),
+            tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(256, activation="relu"),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(28, activation="sigmoid")  # Utilisation de sigmoid pour classification multi-étiquettes
+        ])
+        
+        self.model.compile(
+            loss="binary_crossentropy",  # Pour classification multi-étiquettes
+            optimizer="adam",
+            metrics=["accuracy"]
+        )
+
+        self.model.fit(x_train, y_train, epochs=10, batch_size=28)
+        self.model.summary()
+        return x_test,y_test
+
+    ############################################################################
+    ############################# Test the model ##############################
+    ############################################################################
+    def test_model(self, y_test, x_test):
+        x_test = pad_sequences(x_test, padding="post")
+        return self.model.evaluate(x_test, y_test)
+
+    ############################################################################
+    ############################ Predict the result ############################
+    ############################################################################
+    def predict(self, x_text):
+        x_text = pad_sequences(x_text, padding="post")
+        return self.model.predict(x_text)
+
+    ############################################################################
+    ############################ Analyze the texts  ############################
+    ############################################################################
+    def analyze_texts(self, x, labels):
+        predict = self.predict(x)
+        print(f"Émotion prédite : {labels[predict]}\n")
+
+
+    ############################################################################
+    ############################## Save bot ####################################
+    ############################################################################
+    def accuracy(self,y_predict,y_test):
+        print(f"La précision sur le jeu de test : {accuracy_score(y_test,y_predict) * 100:.2f}%")
+    
+    ############################################################################
+    ############################## Save bot ####################################
+    ############################################################################
+    def save_model(self):
+        self.model.save("./components/models/sentiment_analysis/dlsta_model")
+
+    def load_model(self):
+        self.model = tf.keras.models.load_model("./components/models/sentiment_analysis/dlsta_model")
